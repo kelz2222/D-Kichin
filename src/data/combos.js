6f5465758bf89df75e6875dff2e9014d6c79835c
kelz2222 <kelechicchukwu34@gmail.com>
2026-07-07 07:35:04 +0000

    Create combos.js

---
+++ src/data/combos.js
@@ -0,0 +1,23 @@
+export const COMBOS = [
+  {
+    id: 'match-day',
+    label: 'The Match Day Combo',
+    emoji: '⚽',
+    dishNames: ['Pizza', 'Sobolo'],
+    discountPercent: 10,
+  },
+  {
+    id: 'local-feast',
+    label: 'The Local Feast',
+    emoji: '🍛',
+    dishNames: ['Jollof Rice', 'Spring Rolls', 'Sobolo'],
+    discountPercent: 10,
+  },
+  {
+    id: 'quick-bite',
+    label: 'The Quick Bite',
+    emoji: '🌯',
+    dishNames: ['Shawarma', 'Samosa'],
+    discountPercent: 10,
+  },
+]
